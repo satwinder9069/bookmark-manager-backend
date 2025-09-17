51fa863c7760600072750a6bf09e9748788461ed
Satwinder Kaur <chauhan.satwinder04@gmail.com>
2025-09-18 00:04:19 +0530

    fix: bind server to 0.0.0.0 for Render

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ connectDB();
 //     res.send('hello from the backend!!');
 // });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);   
 });
